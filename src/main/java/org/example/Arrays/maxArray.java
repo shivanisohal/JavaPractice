176e8fcd9dd627707350a99729f4c73b61a47f51
Shivanisohal <sohalshivani2@gmail.com>
2025-09-17 08:08:18 +0530

    javapractice

--- src/main/java/org/example/Arrays/maxArray.java
+++ src/main/java/org/example/Arrays/maxArray.java
@@ -2,17 +2,43 @@ package org.example.Arrays;
 
 public class maxArray {
     public static void main(String[] args) {
+        int arry[] = {22, 51, 44, 76, 44, 99, 12, 43};
 
-        int arr[] = {22, 51, 44, 76, 44, 99, 12, 43};
-        int max = arr[0];
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
+        int maxoutput = maxarray(arry);
+        System.out.println("The maximum value in the array is: " + maxoutput);
+
+        int minoutput= minarray(arry);
+        System.out.println("The minimum value in the array is: " + minoutput);
+
+
+    }
+
+    public static int minarray(int[] arry) {
+        int min = arry[0];
+        for (int i = 0; i < arry.length; i++) {
+            if (arry[i] < min) {
+                min = arry[i];
+
+            }
+        }
+        return min;
+    }
+
+
+
+
+    public static int maxarray(int arry[]) {
+
+        int max = arry[0];
+        for (int i = 0; i < arry.length; i++) {
+            if (arry[i] > max) {
+                max = arry[i];
 
             }
 
         }
-        System.out.println(max);
+       return max;
+
 
     }
 }
